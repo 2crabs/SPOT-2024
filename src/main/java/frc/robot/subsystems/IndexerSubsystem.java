f4ae3284560574386b2b8cc2925566d72447e92f
temp-t <nordlandkris@gmail.com>
2024-02-12 15:36:34 -0600

    Added Indexer

---
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -0,0 +1,45 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IndexerSubsystem extends SubsystemBase{
+
+    public CANSparkMax motor;
+    public IndexerSubsystem() {
+        motor = new CANSparkMax(14, MotorType.kBrushless);
+        motor.setSmartCurrentLimit(25);
+    }
+
+    /**
+     * Example command factory method.
+     *
+     * @return a command
+     */
+    public void runWithSpeed(double input) {
+        motor.set(input);
+    }
+
+    /**
+     * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+     *
+     * @return value of some boolean subsystem state, such as a digital sensor.
+     */
+    public boolean exampleCondition() {
+        // Query some boolean state, such as a digital sensor.
+        return false;
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run during simulation
+    }
+}

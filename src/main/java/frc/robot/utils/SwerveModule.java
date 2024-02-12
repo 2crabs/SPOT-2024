75e1c89548e5b9346f4f1b24a8637fa834a175d6
temp-t <nordlandkris@gmail.com>
2024-02-12 15:37:19 -0600

    Updated SwerveModule to 2024

--- src/main/java/frc/robot/utils/SwerveModule.java
+++ src/main/java/frc/robot/utils/SwerveModule.java
@@ -1,15 +1,17 @@
 package frc.robot.utils;
 
-import com.ctre.phoenix.sensors.AbsoluteSensorRange;
-import com.ctre.phoenix.sensors.CANCoder;
-import com.ctre.phoenix.sensors.CANCoderConfiguration;
-import com.ctre.phoenix.sensors.SensorInitializationStrategy;
-import com.ctre.phoenix.sensors.SensorTimeBase;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.CANcoderConfigurator;
+import com.ctre.phoenix6.configs.MagnetSensorConfigs;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.ControlType;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
 
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -25,7 +27,7 @@ public class SwerveModule {
 
   private final CANSparkMax wheelMotor;
   private final RelativeEncoder wheelEncoder;
-  private final SparkMaxPIDController wheelPID;
+  private final SparkPIDController wheelPID;
   private final SimpleMotorFeedforward wheelFeedforward;
 
   private final CANSparkMax turnMotor;
@@ -33,9 +35,9 @@ public class SwerveModule {
    * Integrated encoder for the angle motor. When getPosition is called it returns <b>the rotation of the module</b> (not the actual motor)
    */
   private final RelativeEncoder turnEncoder;
-  private final SparkMaxPIDController turnPID;
+  private final SparkPIDController turnPID;
   
-  private final CANCoder turnCANCoder;
+  private final CANcoder turnCANCoder;
   private final double CANCoderOffsetDegrees;
 
   /**
@@ -43,8 +45,8 @@ public class SwerveModule {
    */
   public SwerveModule(SwerveModuleConstants moduleConstants) {
     
-    turnMotor = new CANSparkMax(moduleConstants.turnMotorID, MotorType.kBrushless);
-    wheelMotor = new CANSparkMax(moduleConstants.wheelMotorID, MotorType.kBrushless);
+    turnMotor = new CANSparkMax(moduleConstants.turnMotorID, CANSparkLowLevel.MotorType.kBrushless);
+    wheelMotor = new CANSparkMax(moduleConstants.wheelMotorID, CANSparkLowLevel.MotorType.kBrushless);
     
     wheelPID = wheelMotor.getPIDController();
     turnPID = turnMotor.getPIDController();
@@ -52,7 +54,7 @@ public class SwerveModule {
     wheelEncoder = wheelMotor.getEncoder();
     turnEncoder = turnMotor.getEncoder();
 
-    turnCANCoder = new CANCoder(moduleConstants.canCoderID);
+    turnCANCoder = new CANcoder(moduleConstants.canCoderID);
     CANCoderOffsetDegrees = moduleConstants.canCoderOffsetDegrees;
 
     wheelFeedforward = new SimpleMotorFeedforward(Constants.kSwerve.DRIVE_KS, Constants.kSwerve.DRIVE_KV, Constants.kSwerve.DRIVE_KA);
@@ -74,16 +76,19 @@ public class SwerveModule {
     }
     //sets pid target for the rotation of the module
     turnPID.setReference(state.angle.getRotations(), CANSparkMax.ControlType.kPosition);
+    
+    SmartDashboard.putNumber(String.valueOf(CANCoderOffsetDegrees) + " Angle With Offset Accounted For", (turnCANCoder.getAbsolutePosition().getValueAsDouble() * 360) + CANCoderOffsetDegrees);
   }
 
   public SwerveModuleState getState() {
+    SmartDashboard.putNumber(String.valueOf(CANCoderOffsetDegrees), turnCANCoder.getAbsolutePosition().getValueAsDouble() * 360);
     double velocity = wheelEncoder.getVelocity();
     Rotation2d angle = new Rotation2d(turnEncoder.getPosition() * Constants.kSwerve.ANGLE_ROTATIONS_TO_RADIANS);
     return new SwerveModuleState(velocity, angle);
   }
 
   public double getCANCoderPosition() {
-    return turnCANCoder.getAbsolutePosition();
+    return turnCANCoder.getAbsolutePosition().getValueAsDouble();
   }
 
   /**
@@ -138,17 +143,25 @@ public class SwerveModule {
     wheelPID.setReference(0, ControlType.kDutyCycle);
 
     // CanCoder configuration.
-    CANCoderConfiguration canCoderConfiguration = new CANCoderConfiguration();
+
+    MagnetSensorConfigs CANcoderConfig = new MagnetSensorConfigs()
+      .withSensorDirection(SensorDirectionValue.CounterClockwise_Positive)
+      .withAbsoluteSensorRange(AbsoluteSensorRangeValue.Unsigned_0To1);
+    turnCANCoder.getConfigurator().apply(CANcoderConfig);
+
+    /*
     canCoderConfiguration.absoluteSensorRange = AbsoluteSensorRange.Unsigned_0_to_360;
     canCoderConfiguration.sensorDirection = Constants.kSwerve.CAN_CODER_REVERSED;
     canCoderConfiguration.initializationStrategy = SensorInitializationStrategy.BootToAbsolutePosition;
     canCoderConfiguration.sensorTimeBase = SensorTimeBase.PerSecond;
     
     turnCANCoder.configFactoryDefault();
+    canCoderConfiguration.
     turnCANCoder.configAllSettings(canCoderConfiguration);
+    */
 
     turnEncoder.setPositionConversionFactor(1/Constants.kSwerve.ANGLE_GEAR_RATIO);
     turnEncoder.setVelocityConversionFactor(1/Constants.kSwerve.ANGLE_GEAR_RATIO);
-    turnEncoder.setPosition((turnCANCoder.getAbsolutePosition()+ CANCoderOffsetDegrees)/360.0);
+    turnEncoder.setPosition(((turnCANCoder.getAbsolutePosition().getValueAsDouble()*360.0) + CANCoderOffsetDegrees)/360.0);
   }
 }

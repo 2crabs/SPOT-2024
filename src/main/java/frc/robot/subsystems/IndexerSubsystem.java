5ca4c73760ce06785bd77c7019720f58d5bae7c8
Mickey <mickey.fraser@washburnmillerbots.org>
2024-02-15 17:50:00 -0600

    Test PID

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -2,20 +2,33 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkLowLevel;
+import com.revrobotics.CANSparkLowLevel;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.CANSparkBase.ControlType;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class IndexerSubsystem extends SubsystemBase{
 
     public CANSparkMax motor;
     public CANSparkMax motor2;
 
+    public PIDController pid1 = new PIDController(0.00025, 0.00005, 0);
+
     public IndexerSubsystem() {
         motor = new CANSparkMax(15, MotorType.kBrushless);
         motor2 = new CANSparkMax(16, MotorType.kBrushless);
-        motor.setSmartCurrentLimit(35);
-        motor2.setSmartCurrentLimit(35);
+        motor.setSmartCurrentLimit(50);
+        motor2.setSmartCurrentLimit(50);
+        motor2.setInverted(true);
     }
 
     /**
@@ -24,6 +37,9 @@ public class IndexerSubsystem extends SubsystemBase{
      * @return a command
      */
     public void runWithSpeed(double input) {
+        //motor.set(pid1.calculate(motor.getEncoder().getVelocity(), input*2000.0));
+        SmartDashboard.putNumber("encoder", motor.getEncoder().getVelocity());
+        SmartDashboard.putNumber("Shooter PID", pid1.calculate(motor.getEncoder().getVelocity(), input*1000.0));
         motor.set(input);
         motor2.set(input);
     }
@@ -45,6 +61,6 @@ public class IndexerSubsystem extends SubsystemBase{
 
     @Override
     public void simulationPeriodic() {
-        // This method will be called once per scheduler run during simulation
+        
     }
 }

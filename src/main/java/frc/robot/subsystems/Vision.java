c865a4891840aaf597c33a5ea40454081ca7d43d
Washburn <the2crabs@gmail.com>
2024-02-20 15:13:02 -0600

    More Values From Limelight Table

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -19,13 +19,28 @@ public class Vision extends SubsystemBase {
 
   private double targetOffsetX, targetOffsetY, targetArea, targetSkew;
   private double[] botPose;
-  private final NetworkTableEntry m_led_entry;
+  private NetworkTableEntry 
+    target_valid,
+    target_x,
+    target_y,
+    target_area,
+    target_skew,
+    pipeline_latency,
+    image_latency,
+    short_side_length,
+    long_side_length,
+    x_side_length,
+    y_side_length,
+    pipeline_get,
+    pipeline_set,
+    led_mode,
+    cam_mode,
+    streaming_mode;
 
   /** Creates a new Vision. */
   public Vision() {
     m_limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
-
-    m_led_entry = m_limelightTable.getEntry("ledMode");
+    configureEntries();
   }
 
   @Override
@@ -45,6 +60,30 @@ public class Vision extends SubsystemBase {
 
   }
 
+  public void setLEDMode(LimelightLEDMode mode) {
+    led_mode.setNumber(mode.modeID);
+  }
+
+  public LimelightLEDMode getLEDMode() {
+    return LimelightLEDMode.fromModeID(led_mode.getNumber(1).intValue());
+  }
+
+  public int getCurrentPipeline() {
+    return pipeline_get.getNumber(0).intValue();
+  }
+
+  public void setPipeline(int pipelineNumber) {
+    pipeline_set.setNumber(pipelineNumber);
+  }
+
+  public boolean hasValidTarget() {
+    return target_valid.getDouble(0) != 0.0;
+  }
+
+  public double getLatencyMS() {
+    return pipeline_latency.getDouble(0) + image_latency.getDouble(0.0);
+  }
+
   public double getTargetOffsetX() {
     return targetOffsetX;
   }
@@ -58,9 +97,64 @@ public class Vision extends SubsystemBase {
     return targetSkew;
   }
 
+  public double getTargetShortSideLength() {
+    return short_side_length.getDouble(0);
+  }
+  public double getTargetLongSideLength() {
+    return long_side_length.getDouble(0);
+  }
+  public double getTargetXSideLength() {
+    return x_side_length.getDouble(0);
+  }
+  public double getTargetYSideLength() {
+    return y_side_length.getDouble(0);
+  }
+
   public Pose2d getBotPose() {
     Rotation2d rotation = new Rotation2d(botPose[5]);
     Translation2d translation = new Translation2d(botPose[0],botPose[2]);
     return new Pose2d(translation, rotation);
+  }
+
+  public enum LimelightLEDMode {
+    PIPELINE(0), OFF(1), BLINK(2), ON(3);
+
+    public final int modeID;
+    private static final LimelightLEDMode[] values = values();
+
+    private LimelightLEDMode(int modeID) {
+      this.modeID = modeID;
+    }
+    
+    public static LimelightLEDMode fromModeID(int modeID) {
+      for (LimelightLEDMode mode : values) {
+        if (mode.modeID == modeID) {
+          return mode;
+        }
+      }
+      return null;
+    }
+  }
+
+  public void configureEntries() {
+    target_valid = m_limelightTable.getEntry("tv");
+    target_x = m_limelightTable.getEntry("tx");
+    target_y = m_limelightTable.getEntry("ty");
+    target_area = m_limelightTable.getEntry("ta");
+
+    pipeline_latency = m_limelightTable.getEntry("tl");
+    image_latency = m_limelightTable.getEntry("cl");
+
+    short_side_length = m_limelightTable.getEntry("tshort");
+    long_side_length = m_limelightTable.getEntry("tlong");
+    x_side_length = m_limelightTable.getEntry("thor");
+    y_side_length = m_limelightTable.getEntry("tvert");
+
+    pipeline_get = m_limelightTable.getEntry("getpipe");
+    pipeline_set = m_limelightTable.getEntry("pipeline");
+
+    led_mode = m_limelightTable.getEntry("ledMode");
+    cam_mode = m_limelightTable.getEntry("camMode");
+    streaming_mode = m_limelightTable.getEntry("stream");
   }
 }

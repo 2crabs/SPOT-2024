0b5b08bc50f9d37f4930dad447dc1c346e429777
Washburn <the2crabs@gmail.com>
2024-02-29 15:40:38 -0600

    Changed Indexer to TalonFX

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -1,16 +1,17 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkBase.IdleMode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IndexerSubsystem extends SubsystemBase{
 
-    public CANSparkMax indexerMotor;
+    // public CANSparkMax indexerMotor;
+    public TalonFX indexerMotor;
     public IndexerSubsystem() {
-        indexerMotor = new CANSparkMax(18, MotorType.kBrushless);
+        indexerMotor = new TalonFX(18);
     }
 
     /**
@@ -43,7 +44,11 @@ public class IndexerSubsystem extends SubsystemBase{
 
     /** This configures the motor controllers */
     public void configureHardware() {
-        indexerMotor.setSmartCurrentLimit(35);
-        indexerMotor.setIdleMode(IdleMode.kCoast);
+        TalonFXConfiguration motorConfig = new TalonFXConfiguration();
+
+        motorConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+        motorConfig.CurrentLimits.SupplyCurrentLimit = 8;
+
+        indexerMotor.getConfigurator().apply(motorConfig);
     }
 }
